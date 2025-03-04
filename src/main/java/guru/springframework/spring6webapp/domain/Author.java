84eb769368a05f5114b67ee7ada259693ba0912e
Harsh <harsh.khunt@einfochips.com>
2025-03-04 12:43:33 +0530

    add bootstrap class

--- src/main/java/guru/springframework/spring6webapp/domain/Author.java
+++ src/main/java/guru/springframework/spring6webapp/domain/Author.java
@@ -1,5 +1,6 @@
 package guru.springframework.spring6webapp.domain;
 
+import java.util.HashSet;
 import java.util.Objects;
 import java.util.Set;
 
@@ -20,7 +21,7 @@ public class Author {
   private String lastname;
 
   @ManyToMany(mappedBy = "authors")
-  private Set<Book> books;
+  private Set<Book> books = new HashSet<>();
 
   public Set<Book> getBooks() {
     return books;
--- src/main/java/guru/springframework/spring6webapp/domain/Book.java
+++ src/main/java/guru/springframework/spring6webapp/domain/Book.java
@@ -1,5 +1,6 @@
 package guru.springframework.spring6webapp.domain;
 
+import java.util.HashSet;
 import java.util.Objects;
 import java.util.Set;
 
@@ -23,7 +24,7 @@ public class Book {
 
   @ManyToMany
   @JoinTable(name = "author_book", joinColumns = @JoinColumn(name = "book_id"), inverseJoinColumns = @JoinColumn(name = "author_id"))
-  private Set<Author> authors;
+  private Set<Author> authors = new HashSet<>();
 
   public Set<Author> getAuthors() {
     return authors;
