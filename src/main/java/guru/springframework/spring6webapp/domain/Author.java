2761e7d212b6d090ad1571f796aaaecacac727c9
Harsh <harsh.khunt@einfochips.com>
2025-03-04 10:26:21 +0530

    adding relationsheep between author and book

--- src/main/java/guru/springframework/spring6webapp/domain/Author.java
+++ src/main/java/guru/springframework/spring6webapp/domain/Author.java
@@ -1,9 +1,12 @@
 package guru.springframework.spring6webapp.domain;
 
+import java.util.Set;
+
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.ManyToMany;
 
 @Entity
 public class Author {
@@ -14,6 +17,17 @@ public class Author {
 
   private String firstname;
   private String lastname;
+
+  @ManyToMany(mappedBy = "authors")
+  private Set<Book> books;
+
+  public Set<Book> getBooks() {
+    return books;
+  }
+
+  public void setBooks(Set<Book> books) {
+    this.books = books;
+  }
 
   public Long getId() {
     return id;
--- src/main/java/guru/springframework/spring6webapp/domain/Book.java
+++ src/main/java/guru/springframework/spring6webapp/domain/Book.java
@@ -1,9 +1,14 @@
 package guru.springframework.spring6webapp.domain;
 
+import java.util.Set;
+
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.JoinTable;
+import jakarta.persistence.ManyToMany;
 
 @Entity
 public class Book {
@@ -14,6 +19,18 @@ public class Book {
 
   private String title;
   private String isbn;
+
+  @ManyToMany
+  @JoinTable(name = "author_book", joinColumns = @JoinColumn(name = "book_id"), inverseJoinColumns = @JoinColumn(name = "author_id"))
+  private Set<Author> authors;
+
+  public Set<Author> getAuthors() {
+    return authors;
+  }
+
+  public void setAuthors(Set<Author> authors) {
+    this.authors = authors;
+  }
 
   public Long getId() {
     return id;
